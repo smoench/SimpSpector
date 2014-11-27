3559cd502b133fb4ef4c5624f2b6cfc08395e5bc
DavidBadura <d.badura@gmx.de>
2014-11-27 14:27:40 +0000

    cleanup composer gadget

--- src/SimpleThings/AppBundle/Gadget/Composer.php
+++ src/SimpleThings/AppBundle/Gadget/Composer.php
@@ -5,9 +5,6 @@
 
 namespace SimpleThings\AppBundle\Gadget;
 
-use Composer\Factory;
-use Composer\Installer;
-use Composer\IO\NullIO;
 use SimpleThings\AppBundle\Workspace;
 use Symfony\Component\Process\ProcessBuilder;
 
@@ -17,38 +14,30 @@ use Symfony\Component\Process\ProcessBuilder;
 class Composer extends AbstractGadget
 {
     /**
+     * @var string
+     */
+    private $composerHome;
+
+    /**
+     * @param string $composerHome
+     */
+    function __construct($composerHome = '/tmp/.composer')
+    {
+        $this->composerHome = $composerHome;
+    }
+
+    /**
      * @param Workspace $workspace
      * @return mixed
      */
     public function run(Workspace $workspace)
     {
-        /* first test... not working... tricky
-
-        $composerJson = $this->getComposerJsonPath($workspace);
-
-        if (!file_exists($composerJson)) {
-            return false;
-        }
-
-        putenv('COMPOSER_CACHE_DIR=/tmp/composer');
-
-
-        $io = new NullIO();
-
-        $composer = Factory::create($io, $composerJson);
-        $installer = Installer::create($io, $composer);
-
-        $installer->run();
-
-        return true;
-        */
-
         $processBuilder = new ProcessBuilder(['composer', 'install', '--no-interaction']);
         $processBuilder->setWorkingDirectory($workspace->path);
 
         $process = $processBuilder->getProcess();
         $process->setTimeout(3600);
-        $process->setEnv(['COMPOSER_HOME' => $workspace->path]);
+        $process->setEnv(['COMPOSER_HOME' => $this->composerHome]);
         $process->run();
     }
 
@@ -58,14 +47,5 @@ class Composer extends AbstractGadget
     public function getName()
     {
         return 'composer';
-    }
-
-    /**
-     * @param Workspace $workspace
-     * @return string
-     */
-    private function getComposerJsonPath(Workspace $workspace)
-    {
-        return $workspace->path . '/composer.json';
     }
 }
