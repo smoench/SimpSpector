f0f86f4e8f5475d1c3bb3f850bdb15ad07277344
Lars Wallenborn <lars@wallenborn.net>
2014-11-28 18:05:59 +0000

    injected router into Button generator

--- src/SimpleThings/AppBundle/ButtonGenerator.php
+++ src/SimpleThings/AppBundle/ButtonGenerator.php
@@ -3,18 +3,36 @@
 namespace SimpleThings\AppBundle;
 
 use SimpleThings\AppBundle\Entity\MergeRequest;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
 /**
  * @author David Badura <d.a.badura@gmail.com>
+ * @author Lars Wallenborn <lars@wallenborn.net>
  */
 class ButtonGenerator
 {
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
+     * @param Router $router
+     */
+    function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
     /**
      * @param MergeRequest $mergeRequest
      * @return string
      */
     public function generate(MergeRequest $mergeRequest)
     {
-        return 'TODO .... generate button...';
+        return vsprintf('[![Build Status](%(imageUrl)s)](%(linkUrl)s)', [
+            'imageUrl' => '',
+            'linkUrl' => '',
+        ]);
     }
-} 
+}
