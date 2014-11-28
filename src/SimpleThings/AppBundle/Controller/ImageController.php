7f521a58fc682c005ce8b4fa1df0178f0ad16f60
Lars Wallenborn <lars@wallenborn.net>
2014-11-28 20:11:56 +0000

    :q!added xml for svg and returned it (for fixed score 50)

--- src/SimpleThings/AppBundle/Controller/ImageController.php
+++ src/SimpleThings/AppBundle/Controller/ImageController.php
@@ -5,7 +5,7 @@
 
 namespace SimpleThings\AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use SimpleThings\AppBundle\Entity\MergeRequest;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +18,32 @@ class ImageController extends Controller
     /**
      * @Route("/{id}", name="image")
      */
-    public function show(MergeRequest $mergeRequest)
+    public function showAction(MergeRequest $mergeRequest)
     {
-        return new Response('ABC');
+        /* @todo: use $mergeRequest to calculate score */
+
+        $score = 50;
+
+        return new Response($this->renderView("SimpleThingsAppBundle:Image:show.xml.twig", [
+            'score' => $score,
+            'color' => $this->getColor($score),
+        ]), 200, [
+            'Content-Type'        => 'image/svg',
+            'Content-Disposition' => 'inline; filename="status.svg"'
+        ]);
+    }
+
+    /**
+     * @param int $n
+     * @return string
+     */
+    private function getColor($n)
+    {
+        $n = 100 - $n;
+        $r = (255 * $n) / 100;
+        $g = (255 * (100 - $n)) / 100;
+        $b = 0;
+
+        return sprintf('%02X%02X%02X', $r, $g, $b);
     }
 }
