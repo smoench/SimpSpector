1577a237901dbfdbfd085859687f6c49a4b2606b
Tobias Olry <tobias.olry@gmail.com>
2014-11-29 15:20:57 +0100

    test directory

---
+++ tests/src/SimpleThings/AppBundle/Tests/Gadget/SimpSpectorGadget/SimpSpectorGadgetTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SimpleThings\AppBundle\Tests\Gadget\SimpSpectorGadget;
+
+/*
+ * @author Tobias Olry <tobias.olry@gmail.com>
+ */
+class SimpSpectorGadgetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(false);
+    }
+}
+
