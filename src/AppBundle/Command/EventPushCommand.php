02dbb99036a8654e71a149c3dbe3a5ee7c96f9c3
Tobias Olry <tobias.olry@gmail.com>
2015-08-29 13:49:54 +0200

    simple use case works - data is written to database

--- src/AppBundle/Command/EventPushCommand.php
+++ src/AppBundle/Command/EventPushCommand.php
@@ -11,6 +11,10 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
+use DavidBadura\GitWebhooks\Event\PushEvent;
+use DavidBadura\GitWebhooks\Struct\Commit;
+use DavidBadura\GitWebhooks\Struct\Repository;
+
 class EventPushCommand extends ContainerAwareCommand
 {
     private $input;
@@ -20,10 +24,10 @@ class EventPushCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName('simpspector:event:push')
-            ->addOption('project', 'project name', InputOption::VALUE_OPTIONAL, null)
-            ->addOption('url', 'repository url', InputOption::VALUE_OPTIONAL, null)
-            ->addOption('commit-id', 'commit-hash of last commit', InputOption::VALUE_OPTIONAL, '')
-        ;
+            ->addOption('project', null, InputOption::VALUE_OPTIONAL, 'project name', null)
+            ->addOption('project-id', null, InputOption::VALUE_OPTIONAL, 'project id', 'test-9999')
+            ->addOption('url', null, InputOption::VALUE_OPTIONAL, 'repository url', null)
+            ->addOption('commit', null, InputOption::VALUE_OPTIONAL, 'commit-hash of last commit', '');
     }
 
     /**
@@ -38,9 +42,30 @@ class EventPushCommand extends ContainerAwareCommand
         $this->output         = $output;
         $this->questionHelper = $this->getHelper('question');
 
-        $project  = $this->getOption('project', 'project', null);
-        $url      = $this->getOption('url', 'repository url', null);
-        $commitId = $this->getOption('commit-id', 'commit hash of last commit', null);
+        $handler = $this->getContainer()->get('simpspector.app.webhook.handler');
+
+        $url        = $this->getOption('url', 'repository url', null);
+        $commitHash = $this->getOption('commit', 'commit hash of last commit', null);
+        $project    = $this->getOption('project', 'project name', null);
+        $projectId  = $input->getOption('project-id');
+
+        $event = new PushEvent();
+        $event->type       = PushEvent::TYPE_BRANCH;
+        $event->branchName = 'master';
+        $event->repository = new Repository();
+
+        $event->repository->id   = $projectId;
+        $event->repository->url  = $url;
+        $event->repository->name = $project;
+
+        $commit = new Commit();
+        $commit->id = $commitHash;
+        $commit->message = "Test-Data for Commit " . $commitHash;
+        $commit->date = new \DateTime(); // todo correct timestamp
+
+        $event->commits = [$commit];
+
+        $handler->handle($event);
     }
 
     private function getOption($key, $label, $description)
