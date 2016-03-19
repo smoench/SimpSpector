ca7a4ed1a9e368c2c2cd346f9f4043791041693e
Tobias Olry <tobias.olry@gmail.com>
2016-03-19 16:08:34 +0100

    improved type hinting

--- src/AppBundle/Controller/CommitController.php
+++ src/AppBundle/Controller/CommitController.php
@@ -70,8 +70,8 @@ class CommitController extends Controller
      * @Framework\Route("/{from}/diff/{to}", name="commit_diff")
      * @Framework\Template()
      *
-     * @param $from
-     * @param $to
+     * @param string $from
+     * @param string $to
      *
      * @return array
      */
